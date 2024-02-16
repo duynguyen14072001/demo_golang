b4b117c0f2b0c5a45c628dab920838c38a02d075
DuyNguyen <duy.nguyenanh01@gmail.com>
2024-02-16 14:15:18 +0700

    update connect

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 
 func main() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-	// dsn := "food_delivery:buonlamchiemoi147@tcp(127.0.0.1:3307)/food_delivery?charset=utf8mb4&parseTime=True&loc=Local"
 	dsn := os.Getenv("CONNECTION_STRING")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
